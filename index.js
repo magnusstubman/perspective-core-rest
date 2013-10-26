ef101e38eb2da6b0d49fc7f1211864f8efb7f056
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-26 21:39:24 +0200

    Moved rest to own repo

---
+++ index.js
@@ -0,0 +1,9 @@
+var error = require('./lib/error');
+var createServer = require('./lib/server');
+var produceConfig = require('./lib/config');
+
+module.exports = {
+	error: error,
+	createServer: createServer,
+	produceConfig: produceConfig
+}
